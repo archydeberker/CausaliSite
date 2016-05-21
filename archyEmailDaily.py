05b7c6b0db67dc333c727be38b73c83872798c31
berkerboy <archy@dhcp-57-185.xlate.eduroam.ucl.ac.uk>
2016-05-21 18:26:26 +0100

    archy daily email script

--- archyEmailDaily.py
+++ archyEmailDaily.py
@@ -3,13 +3,15 @@ import sys
 import logging
 from postmark import PMMail
 from django.conf import settings
+import datetime
+
 
 
 message = PMMail(api_key = os.environ.get('POSTMARK_API_TOKEN'),
                  subject = "Email job",
                  sender = "a@deberker.com",
                  to = "a@deberker.com",
-                 text_body = "Sent every ten minutes",
+                 text_body = "Sent at " + datetime.datetime.now().time(),
                  tag = "hello")
 
 settings.configure()
