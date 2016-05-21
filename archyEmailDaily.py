6b54254004591ad71e319b674c144ea9d095e781
berkerboy <archy@dhcp-57-185.xlate.eduroam.ucl.ac.uk>
2016-05-21 18:29:03 +0100

    archy daily email script

--- archyEmailDaily.py
+++ archyEmailDaily.py
@@ -11,7 +11,7 @@ message = PMMail(api_key = os.environ.get('POSTMARK_API_TOKEN'),
                  subject = "Email job",
                  sender = "a@deberker.com",
                  to = "a@deberker.com",
-                 text_body = "Sent at " + datetime.datetime.now().time(),
+                 text_body = "Sent at " + datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') ,
                  tag = "hello")
 
 settings.configure()
