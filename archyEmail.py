5d7375d1593b342de0203db0259dfec3c5582aa6
berkerboy <archy@dhcp-57-185.xlate.eduroam.ucl.ac.uk>
2016-05-21 15:15:44 +0100

    archyEmail.py

--- archyEmail.py
+++ archyEmail.py
@@ -8,5 +8,4 @@ message = PMMail(api_key = os.environ.get('POSTMARK_API_TOKEN'),
                  text_body = "Hello",
                  tag = "hello")
 
-settings.configure() # for Django?
 message.send()
