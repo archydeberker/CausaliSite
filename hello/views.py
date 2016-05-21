8d7ac583c7e3a45bcc6937a033366bb0a9cbdbb6
Zeb Kurth-Nelson <zknelson@fil.ion.ucl.ac.uk>
2016-05-21 14:47:27 +0100

    copied from getting-started

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .forms import NameForm
 
 # Create your views here.
 def index(request):
@@ -18,3 +19,21 @@ def db(request):
 
     return render(request, 'db.html', {'greetings': greetings})
 
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
