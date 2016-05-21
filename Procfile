7a8a641f455806b6f7b3b63b6e1e5b7871dec2fc
berkerboy <archy@dhcp-57-185.xlate.eduroam.ucl.ac.uk>
2016-05-21 16:17:56 +0100

    ss

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn gettingstarted.wsgi --log-file -
+email: python archyEmail.py
--- hello/views.py
+++ hello/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
-from .forms import NameForm
+
 
 # Create your views here.
 def index(request):
@@ -20,20 +20,21 @@ def db(request):
     return render(request, 'db.html', {'greetings': greetings})
 
 
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
+def email_view(request):
 
-    return render(request, 'name.html', {'form': form})
+    import os
+    from postmark import PMMail
+    from django.conf import settings
+
+    message = PMMail(api_key = os.environ.get('POSTMARK_API_TOKEN'),
+        subject = "Hello from Postmark",
+        sender = "a@deberker.com",
+        to = "a@deberker.com",
+        text_body = "Hello",
+        tag = "hello")
+
+
+    message.send()
+
+    return  HttpResponse('Email Sent Successfully')
