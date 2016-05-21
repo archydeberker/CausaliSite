62493da47e228d0f68ebe9749a816cfc9a7de8ee
berkerboy <archy@dhcp-57-185.xlate.eduroam.ucl.ac.uk>
2016-05-21 18:40:58 +0100

    archy added meditation randomisation, push to db

--- archyEmailDaily.py
+++ archyEmailDaily.py
@@ -4,17 +4,26 @@ import logging
 from postmark import PMMail
 from django.conf import settings
 import datetime
+import random
 
-
+choiceStr = ["SHOULD","SHOULDN'T"]
+meditate = random.randrand(0,2)
 
 message = PMMail(api_key = os.environ.get('POSTMARK_API_TOKEN'),
                  subject = "Email job",
                  sender = "a@deberker.com",
                  to = "a@deberker.com",
-                 text_body = "Sent at " + datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') ,
+                 text_body = "Sent at " + datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') ". Today you " choiceStr[meditate] "meditate!" ,
                  tag = "hello")
 
 settings.configure()
 
 message.send()
+
+import pymongo
+uri = os.environ['MONGO_URI']
+client = pymongo.MongoClient(uri)
+usersCol = client['zapscience'].users
+usersCol.insert({'name': "archy", 'email': "a@deberker.com",'meditate_today':meditate)
+client.close()
 
