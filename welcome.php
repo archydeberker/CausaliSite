8ebd975498122998bb215c1a278915319debd598
Zeb Kurth-Nelson <zknelson@fil.ion.ucl.ac.uk>
2016-05-21 17:16:30 +0100

    m

--- welcome.php
+++ welcome.php
@@ -12,7 +12,7 @@ $message = "Line 1\r\nLine 2\r\nLine 3";
 $message = wordwrap($message, 70, "\r\n");
 
 // Send
-mail('zebkurthnelson@gmail.com', 'My Subject', $message);
+exec('python archyEmail.py');
 ?>
 
 </body>
