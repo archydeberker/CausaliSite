f215bd96a20f9dca875d1ca7b6b38135e33d3669
berkerboy <archy@Administrators-MacBook-Air-2.local>
2016-05-31 21:37:20 +0100

    email link

--- ProbeEmail.py
+++ ProbeEmail.py
@@ -15,18 +15,21 @@ import database.db_utils as db_utils
 
 mostofpath= """<a href="https://zapscience.herokuapp.com/sendresults.php?trialhash=""" + sys.argv[3] + "&rating="
 restofpath= """><img src="https://zapscience.herokuapp.com/star.png"></a>"""
+bodyText = "<html><body> Sup. How'd it go?<br>" +\
+	mostofpath + "1\""  + restofpath +\
+	mostofpath + "2\""  + restofpath +\
+	mostofpath + "3\""  + restofpath +\
+	mostofpath + "4\""  + restofpath +\
+	mostofpath + "5\""  + restofpath + "</body> </html>"
+
+print bodyText
 
 message = PMMail(api_key = os.environ.get('POSTMARK_API_TOKEN'),
 	#,'4322111a-0d75-4777-8111-2d83f0664762'
                  subject = "Welcome to Causali!" ,
                  sender = "a@deberker.com",
                  to = sys.argv[2],
-                 html_body = """<html><body> Sup. How'd it go?<br>""" +\
-	mostofpath + "1"  + restofpath +\
-	mostofpath + "2"  + restofpath +\
-	mostofpath + "3"  + restofpath +\
-	mostofpath + "4"  + restofpath +\
-	mostofpath + "5"  + restofpath + "</body> </html>",
+                 html_body = bodyText,
                  tag = "welcome")
 
 settings.configure()
