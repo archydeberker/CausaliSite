c8e499f53b66d24afc9ff4e9f285afb112b973c9
Peter Smittenaar <peter@petersmittenaar.com>
2016-06-03 18:26:58 +0100

    added update_results to scheduled_job

--- database/scheduled_job.py
+++ database/scheduled_job.py
@@ -9,4 +9,5 @@ import database.db_utils as db_utils
 
 db_utils.send_outstanding_instructions()
 db_utils.send_outstanding_response_prompts()
+db_utils.update_results()
 print("Total number of completed trials in collection: %d" % (db_utils.trials_completed())
