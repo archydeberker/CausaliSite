0766d3562addea65e5b7c3d6582abfb4e4d6ca4d
Peter Smittenaar <peter@petersmittenaar.com>
2016-06-03 16:56:20 +0100

    removed django requirement

--- mail/ProbeEmail.py
+++ mail/ProbeEmail.py
@@ -1,6 +1,5 @@
 import os
 from postmark import PMMail
-from django.conf import settings
 
 def ProbeEmail(userName,userEmail,trialHash):
 	"""sends a probe email to a specified user using a trial hash.
@@ -32,6 +31,5 @@ def ProbeEmail(userName,userEmail,trialHash):
 	                 html_body = bodyText,
 	                 tag = "response")
 
-	settings.configure()
 	result = message.send()
 	return result
