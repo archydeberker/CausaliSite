9148c792e5d160531834061d524364a620a62971
Peter Smittenaar <peter@petersmittenaar.com>
2016-06-06 22:11:00 +0100

    debugging php

--- views/welcome_custom_exp.php
+++ views/welcome_custom_exp.php
@@ -38,10 +38,13 @@ $temp10 = !empty($temp10) ? "'$temp10'" : "NULL";
 $temp11 = !empty($temp11) ? "'$temp11'" : "NULL";
 $temp12 = !empty($temp12) ? "'$temp12'" : "NULL";
 
-$command = "from database.db_utils import register_user_experiment; register_user_experiment($temp1, $temp2, $temp3, $temp4, $temp5, $temp6, $temp7, $temp8, $temp9, $temp10, $temp11, $temp12)";
-echo "<script>console.log(""$command"");</script>";
+// $command = "from database.db_utils import register_user_experiment; register_user_experiment($temp1, $temp2, $temp3, $temp4, $temp5, $temp6, $temp7, $temp8, $temp9, $temp10, $temp11, $temp12)";
+// echo "<script>console.log(""$command"");</script>";
 // exec("python ../mail/archyEmail.py $temp1 $temp2");
-exec("python -c $command");
+// exec("python -c $command");
+
+// first argument is the function inside php_to_python, next are all the arguments
+exec("python ../php_to_python.py register_user_experiment $temp1, $temp2, $temp3, $temp4, $temp5, $temp6, $temp7, $temp8, $temp9, $temp10, $temp11, $temp12")
 ?>
 
 <!-- Page content -->
