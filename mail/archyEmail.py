2a56164046a831ff75baff685e3f409c9592c464
Peter Smittenaar <peter@petersmittenaar.com>
2016-06-06 18:08:42 +0100

    archyemail.py now stores user first, then sends email

--- mail/archyEmail.py
+++ mail/archyEmail.py
@@ -2,9 +2,10 @@ import os
 import sys
 import logging
 from postmark import PMMail
-from django.conf import settings
 import database.db_utils as db_utils
 
+# write to database
+db_utils.store_user(name=sys.argv[1], email=sys.argv[2])	
 
 message = PMMail(api_key = os.environ.get('POSTMARK_API_TOKEN'),
 	#,'4322111a-0d75-4777-8111-2d83f0664762'
@@ -14,9 +15,5 @@ message = PMMail(api_key = os.environ.get('POSTMARK_API_TOKEN'),
                  text_body = sys.argv[1],
                  tag = "hello")
 
-settings.configure()
-
 message.send()
 
-# write to database
-db_utils.store_user(name=sys.argv[1], email=sys.argv[2])	
