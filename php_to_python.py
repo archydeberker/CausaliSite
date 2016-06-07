56f33b89d11270adff3b8059f52967fda592c5fa
Peter Smittenaar <peter@petersmittenaar.com>
2016-06-07 17:53:44 +0100

    debug

--- php_to_python.py
+++ php_to_python.py
@@ -8,18 +8,20 @@ This script does not do error/input checking, this is all handled in the respect
 """
 
 import sys
+print('Number of input arguments: %d' % len(sys.argv))
 
 def register_user_experiment(args):
-	from db_utils import register_user_experiment
-	# run the function with each argument passed as-is
-	eval('register_user_experiment(' + ', '.join(args) + ')'
+	from database.db_utils import register_user_experiment
+	# replace all NULL with None
+	args = [None if arg == 'NULL' else arg for arg in args]
 
-
+	success = register_user_experiment(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11])
+	return success
 
 
 # if this script is being ran on its own (rather than, say, being imported)
 if __name__ == "__main__":
 	# run the requested function and pass an array with all arguments, but only if it exists
-	if sys.argv[0]:
-		eval(sys.argv[0] + '(sys.argv[1:])')
+	if sys.argv[1]:
+		eval(sys.argv[1] + '(sys.argv[2:])')
 
