22860103f5fe00afddf5ceee0535f89f0299bdd0
Peter Smittenaar <peter@petersmittenaar.com>
2016-06-08 11:49:27 +0100

    debug

--- php_to_python.py
+++ php_to_python.py
@@ -13,9 +13,6 @@ print('Number of input arguments: %d' % len(sys.argv))
 
 def register_user_experiment(args):
 	from database.db_utils import register_user_experiment
-	# replace all NULL with None
-	args = [None if arg == 'NULL' else arg for arg in args]
-
 	success = register_user_experiment(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11])
 	return success
 
@@ -24,7 +21,10 @@ def register_user_experiment(args):
 if __name__ == "__main__":
 	# run the requested function and pass an array with all arguments, but only if it exists
 	if len(sys.argv)>1:
-		eval(sys.argv[1] + '(sys.argv[2:])')
+		if len(sys.argv)>2: # means there are arguments
+			# replace all NULL with None. args is a list
+			args = [None if arg == 'NULL' else arg for arg in sys.argv[2:]]
+			eval(sys.argv[1] + '(args)')
 	else:
 		print('no function name provided')
 
