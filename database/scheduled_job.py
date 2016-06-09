d864d982dde87edf4011c9dde5b57ef73d25565a
Peter Smittenaar <petersmittenaar+github@gmail.com>
2016-06-09 17:51:25 +0100

    missing bracket

--- database/scheduled_job.py
+++ database/scheduled_job.py
@@ -10,4 +10,4 @@ import database.db_utils as db_utils
 db_utils.send_outstanding_instructions()
 db_utils.send_outstanding_response_prompts()
 db_utils.update_results()
-print("Total number of completed trials in collection: %d" % (db_utils.trials_completed())
+print("Total number of completed trials in collection: %d" % (db_utils.trials_completed()))
